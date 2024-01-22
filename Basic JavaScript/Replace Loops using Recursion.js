68582210f69500e4fb664978ea1c81f57589f2e6
Verminas <34026266+Verminas@users.noreply.github.com>
2024-01-22 21:29:19 +0300

    feat: add file Replace Loops using Recursion.js

---
+++ Basic JavaScript/Replace Loops using Recursion.js
@@ -0,0 +1,17 @@
+// example of loop for product
+function multiply(arr, n) {
+    let product = 1;
+    for (let i = 0; i < n; i++) {
+      product *= arr[i];
+    }
+    return product;
+  }
+
+// example recursion for product
+function multiply(arr, n) {
+    if (n <= 0) {
+      return 1;
+    } else {
+      return multiply(arr, n - 1) * arr[n - 1];
+    }
+  }
