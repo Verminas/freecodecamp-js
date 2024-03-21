fcc27b2fc9429db0ef4b7cfbd0d9ad78368771c1
Verminas <34026266+Verminas@users.noreply.github.com>
2024-03-21 23:24:36 +0300

    Create 04.js

---
+++ Functional Programming/04.js
@@ -0,0 +1,10 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+    let copy = fixedValue;
+    return copy + 1;
+
+  // Only change code above this line
+}
