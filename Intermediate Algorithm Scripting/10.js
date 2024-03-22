19085f35388790d30a69259956f46b782ba81ceb
Verminas <34026266+Verminas@users.noreply.github.com>
2024-03-22 12:34:13 +0300

    Create 10.js

---
+++ Intermediate Algorithm Scripting/10.js
@@ -0,0 +1,18 @@
+function convertHTML(str) {
+  return str.replace(/[&<>"']/g, function (match) {
+    switch (match) {
+      case '&':
+        return '&amp;';
+      case '<':
+        return '&lt;';
+      case '>':
+        return '&gt;';
+      case '"':
+        return '&quot;';
+      case "'":
+        return '&apos;';
+    }
+  });
+}
+
+convertHTML("Dolce & Gabbana");
