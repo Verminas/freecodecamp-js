8b1d12711c9e21e7b686be75561829893eb57006
Verminas <34026266+Verminas@users.noreply.github.com>
2024-03-24 16:56:51 +0300

    Update main.js

--- Cash Register/main.js
+++ Cash Register/main.js
@@ -112,7 +112,11 @@ function checkCashRegister(price, cash, cid) {
   
   if(needToChange > allMoneyInCid) {
     return variantsOfResult.less;
+  } else if(needToChange === allMoneyInCid) {
+    variantsOfResult.equial.change = cid;
+    return variantsOfResult.equial;
   }
 }
+
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
